6cbe376018a405a2ab31a27a0d0428e23265da40
George Bora <gpbora@gmail.com>
2019-06-21 23:44:34 +0300

    worked some more on the slider

--- js/site.js
+++ js/site.js
@@ -13,9 +13,12 @@ $('#gb-tech-vertical').slick({
 });
 
 $('#gb-tech-vertical').on('afterChange', function(event, slick, currentSlide){
-    // $('.slick-current').id;        
     var currentSlideId = document.querySelectorAll('#gb-tech-vertical div.tech-slide-text')[currentSlide].id;
     var currentSlideSubject = currentSlideId.split('-')[0];
     var iconId = '#' + currentSlideSubject + '-icon';
+    var currentlyActiveTech = document.querySelector('.tech-row div.active');
+    if (currentlyActiveTech) {
+        currentlyActiveTech.classList.remove('active');
+    }
     $(iconId).addClass('active');
 });
