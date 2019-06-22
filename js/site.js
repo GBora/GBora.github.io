10420ca867c407486d29c98629c7dda5edfe4455
George Bora <gpbora@gmail.com>
2019-06-22 22:04:39 +0300

    gb: 5 more techs to go

--- js/site.js
+++ js/site.js
@@ -22,3 +22,32 @@ $('#gb-tech-vertical').on('afterChange', function(event, slick, currentSlide){
     }
     $(iconId).addClass('active');
 });
+
+// Get all tech icons
+let techs = $('.gb-tech');
+
+for (var i = 0;  i < techs.length; i++) {
+    var tech = techs[i];
+    // For each tech icon set an on click;
+    tech.addEventListener('click', (event) => {
+        // Reset the active class so that no 2 techs are active at the same time
+        var currentlyActiveTech = document.querySelector('.tech-row div.active');
+        if (currentlyActiveTech) {
+            currentlyActiveTech.classList.remove('active');
+        }
+        // Set this teach icon active
+        document.querySelector('#' + event.target.alt + '-icon').classList.add('active');
+        
+        // Get all slides
+        var verticalSlides = document.querySelectorAll('.tech-slide-text');
+
+        // compare the ids in the vertical slides
+        for (var j = 0; j < verticalSlides.length; j++) {
+            // If the ids are about the same thing go to that slide
+            if (verticalSlides[j].id === event.target.alt + '-text') {
+                $('#gb-tech-vertical').slick('slickGoTo', j);
+            }
+        }
+    })
+}
+
